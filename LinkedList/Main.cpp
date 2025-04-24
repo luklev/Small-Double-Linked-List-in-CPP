78b824a342d5da700e32fcfd14ae09904b9ffabd
Lukas Levien <lukas.levien@mail.de>
2025-04-24 15:45:41 +0200

    changed the values of nodes in the list from integers to templates, which resulted in moving the function definitions from the MyList.cpp file to the MyList.h file

--- LinkedList/Main.cpp
+++ LinkedList/Main.cpp
@@ -5,12 +5,12 @@
 
 int main(void)
 {
-	MyList* test = new MyList(20);
+	MyList<int>* test = new MyList<int>(20);
 	test->prepend(10);
 	test->append(30);
 	test->insert(1, 15);
 	test->insert(3, 25);
-	std::cout << test->print() + "\nHead: " + std::to_string(test->getHead()) + "\nTail: " + std::to_string(test->getTail());
+	std::cout << test->print() + "\nHead: " + std::to_string(test->peekFirst()) + "\nTail: " + std::to_string(test->peekLast());
 
 
 	test->removeAt(1);
@@ -18,7 +18,7 @@ int main(void)
 	test->removeHead();
 	test->removeTail();
 	std::cout << "\n\n\n";
-	std::cout << test->print() + "\nHead: " + std::to_string(test->getHead()) + "\nTail: " + std::to_string(test->getTail());
+	std::cout << test->print() + "\nHead: " + std::to_string(test->peekFirst()) + "\nTail: " + std::to_string(test->peekLast());
 
 
 	test->prepend(10);
@@ -28,10 +28,10 @@ int main(void)
 
 	test->removeValue(20);
 	std::cout << "\n\n\n";
-	std::cout << test->print() + "\nHead: " + std::to_string(test->getHead()) + "\nTail: " + std::to_string(test->getTail());
+	std::cout << test->print() + "\nHead: " + std::to_string(test->peekFirst()) + "\nTail: " + std::to_string(test->peekLast());
 
 
 	test->clear();
 	std::cout << "\n\n\n";
-	std::cout << test->print() + "\nHead: " + std::to_string(test->getHead()) + "\nTail: " + std::to_string(test->getTail());
+	std::cout << test->print() + "\nHead: " + std::to_string(test->peekFirst()) + "\nTail: " + std::to_string(test->peekLast());
 }
--- LinkedList/MyList.h
+++ LinkedList/MyList.h
@@ -8,53 +8,209 @@
 * Has: Add & Remove functions
 * Planned: Iterator, Sorting
 */
+template <typename T>
 class MyList {
 private:
 	struct Node {
-		int val;
+		T val;
 		Node* next;
 		Node* prev;
 
-		Node(int value);
+		Node(T value) {
+			val = value;
+			next = prev = nullptr;
+		}
 	};
 
 	int count = 0;
 	Node* head = nullptr;
 	Node* tail = nullptr;
 
-	void initialNode(int value);
-	bool isEmpty();
-	Node* getNodeByIndex(int index);
-	Node* getNodeByValue(int value);
-	void removeNode(Node* node);
-	void removeLonelyNode();
+	void initialNode(T value) {
+		if (!isEmpty()) return;
+		count = 1;
+		head = new Node(value);
+		tail = head;
+		return;
+	}
+	bool isEmpty() {
+		return (count == 0);
+	}
+	Node* getNodeByIndex(int index) {
+		if (isEmpty() || index < 0 || index >= count) return nullptr;
+
+		if (index == 0) return head;
+		if (index + 1 == count) return tail;
+
+		Node* currentNode = head;
+		for (int i = 0; i < index; i++) {
+			currentNode = currentNode->next;
+		}
+		return currentNode;
+	}
+	Node* getNodeByValue(T value) {
+		if (isEmpty()) return nullptr;
+
+		Node* currentNode = nullptr;
+		for (int i = 0; i < count - 1; i++) {
+			if (i == 0) currentNode = head;
+			else currentNode = currentNode->next;
+
+			if (currentNode->val == value) return currentNode;
+		}
+		return nullptr;
+	}
+	void removeNode(Node* node) {
+		if (count == 1) return removeLonelyNode();
+		Node* nextNode = node->next;
+		Node* prevNode = node->prev;
+
+		nextNode->prev = prevNode;
+		prevNode->next = nextNode;
+		delete node;
+		count--;
+		return;
+	}
+	void removeLonelyNode() {
+		if (isEmpty()) return;
+		if (count != 1) return;
+		delete head;
+		head = tail = nullptr;
+		count--;
+		return;
+	}
 public:
 	//constructors
-	MyList();
-	MyList(int value);
+	MyList() {
+		if (!isEmpty()) return;
+
+		count = 0;
+		head = tail = nullptr;
+	}
+	MyList(T value) {
+		initialNode(value);
+	}
 
 	//add functions
-	void append(int value);
-	void prepend(int value);
-	void insert(int index, int value);
+	void append(T value) {
+		if (isEmpty()) return initialNode(value);
+
+		Node* oldTail = tail;
+		tail = new Node(value);
+		oldTail->next = tail;
+		tail->prev = oldTail;
+		count++;
+		return;
+	}
+	void prepend(T value) {
+		if (isEmpty()) return initialNode(value);
+
+		Node* oldHead = head;
+		head = new Node(value);
+		oldHead->prev = head;
+		head->next = oldHead;
+		count++;
+		return;
+	}
+	void insert(int index, T value) {
+		if (index == 0) return prepend(value);
+		if (index == count) return append(value);
+
+		Node* current = getNodeByIndex(index);
+		if (current == nullptr) return;
+
+		Node* newNode = new Node(value);
+		Node* oldPrev = current->prev;
+		oldPrev->next = newNode;
+		current->prev = newNode;
+		newNode->prev = oldPrev;
+		newNode->next = current;
+		count++;
+		return;
+	}
 
 	//remove functions
-	void removeAt(int index);
-	void removeValue(int value);
-	void removeHead();
-	void removeTail();
-	void clear();
+	void removeAt(int index) {
+		if (index == 0) return removeHead();
+		if (index + 1 == count) return removeTail();
 
-	//iterate funcitons
+		Node* current = getNodeByIndex(index);
+		if (current == nullptr) return;
+
+		return removeNode(current);
+	}
+	void removeValue(T value) {
+		Node* current = getNodeByValue(value);
+		if (current == nullptr) return;
+
+		if (current == head) return removeHead();
+		if (current == tail) return removeTail();
+
+		return removeNode(current);
+	}
+	void removeHead() {
+		if (isEmpty()) return;
+		if (count == 1) return removeLonelyNode();
+
+		Node* newHead = head->next;
+		delete head;
+		newHead->prev = nullptr;
+		head = newHead;
+		count--;
+	}
+	void removeTail(){
+		if (isEmpty()) return;
+		if (count == 1) return removeLonelyNode();
+
+		Node* newTail = tail->prev;
+		delete tail;
+		newTail->next = nullptr;
+		tail = newTail;
+		count--;
+	}
+	void clear() {
+		while (count > 0) {
+			std::cout << count + "\n";
+			removeTail();
+		}
+		return;
+	}
+
+	//iterate functions
 
 
 	//sort functions
 
 
 	//getters
-	int get(int index);
-	int size();
-	std::string print();
-	int getHead();
-	int getTail();
+	T get(int index) {
+		if (index >= count || index < 0) return 0;
+		Node* current = head;
+		for (int i = 0; i < index; i++) {
+			current = current->next;
+		}
+		return current->val;
+	}
+	int size() {
+		return count;
+	}
+	std::string print() {
+		std::string output = "size: " + std::to_string(count) + "\n";
+		if (isEmpty()) return output;
+		Node* current = head;
+		for (int i = 0; i < count - 1; i++) {
+			output += std::to_string(current->val) + " <-> ";
+			current = current->next;
+		}
+		output += std::to_string(tail->val) + "\n";
+		return output;
+	}
+	T peekFirst() {
+		if (count == 0) return 0;
+		return head->val;
+	}
+	T peekLast() {
+		if (count == 0) return 0;
+		return tail->val;
+	}
 };
